318bfa41576aec61fcaecf6e9faf540e8c7e6d67
Alan <ast9501.cs10g@nctu.edu.tw>
2022-07-09 09:28:04 +0000

    chore: output logs of rewrite conf file

--- cmd/cu-config.go
+++ cmd/cu-config.go
@@ -32,45 +32,54 @@ func GetInterfaceIpv4Addr(interfaceName string) (addr string, err error) {
 }
 
 func main() {
-	amfDomainName := "sample.com"
-	duDomainName := "sample.com"
-	localInterfaceName := "ens3"
+	amfDomainName := "free5gc-amf-svc"
+	duDomainName := "oai-du-svc"
+	localInterfaceName := "eth0"
+	filePath := "openairinterface5g/targets/PROJECTS/GENERIC-NR-5GC/CONF/cu_gnb.conf"
 
 	// config local ip address
 	ip, err := GetInterfaceIpv4Addr(localInterfaceName)
 	if err != nil {
 		fmt.Println("[Err]", err)
 	}
-	fmt.Println("Config local interface ip: ", ip)
-	mod := "41c\\    local_s_address = \"" + ip + "\";"
-	cmd := exec.Command("sed", "-i", mod, "cu_gnb.conf")
+	//fmt.Println("Config local interface ip: ", ip)
+	mod = "41c\\    local_s_address = \"" + ip + "\";"
+	cmd = exec.Command("sed", "-i", mod, filePath)
 	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set interface ip: ", ip)
 	}
 
 	// TODO: Add fixed virtual ip as ip
 	ip = "10.244.1.2"
 	mod = "204c\\        GNB_IPV4_ADDRESS_FOR_NG_AMF              = \"" + ip + "/16\";"
-	cmd = exec.Command("sed", "-i", mod, "cu_gnb.conf")
+	cmd = exec.Command("sed", "-i", mod, filePath)
 	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set virtual ip: ", ip)
 	}
 
 	mod = "206c\\        GNB_IPV4_ADDRESS_FOR_NGU                 = \"" + ip + "/16\";"
-	cmd = exec.Command("sed", "-i", mod, "cu_gnb.conf")
+	cmd = exec.Command("sed", "-i", mod, filePath)
 	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set virtual ip: ", ip)
 	}
 
 	// config local interface name
 	mod = "40c\\    local_s_if_name = \"" + localInterfaceName + "\";"
-	cmd = exec.Command("sed", "-i", mod, "cu_gnb.conf")
+	cmd = exec.Command("sed", "-i", mod, filePath)
 	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set local interface name: ", localInterfaceName)
 	}
 
 	// config AMF ip address
@@ -78,12 +87,14 @@ func main() {
  	for _, amfIp := range iprecords {
 		modAmfAddr := "193c\\        amf_ip_address      = ( { ipv4       = \"" + amfIp.String() + "\";"
 		//fmt.Println(modAmfAddr)
-  		cmd = exec.Command("sed", "-i", modAmfAddr, "cu_gnb.conf")
+  		cmd = exec.Command("sed", "-i", modAmfAddr, filePath)
 		_, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println("[Err]", err)
+		} else {
+			fmt.Println("[LOG]Config AMF ip: ", amfIp)
 		}
-		fmt.Println("Config AMF ip: ", amfIp)
+		
  	}
 
 	// config du ip address
@@ -91,11 +102,12 @@ func main() {
  	for _, duIp := range iprecords {
 		modDuAddr := "42c\\    remote_s_address = \"" + duIp.String() + "\";"
 		//fmt.Println(modAmfAddr)
-  		cmd = exec.Command("sed", "-i", modDuAddr, "cu_gnb.conf")
+  		cmd = exec.Command("sed", "-i", modDuAddr, filePath)
 		_, err := cmd.CombinedOutput()
 		if err != nil {
 			fmt.Println("[Err]", err)
+		} else {
+			fmt.Println("[LOG]Config DU ip: ", duIp)
 		}
-		fmt.Println("Config DU ip: ", duIp)
  	}
 }
