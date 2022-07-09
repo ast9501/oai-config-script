1f0c5984ef81b19a7ea316a7b4ca4039027df5a6
Alan <ast9501.cs10g@nctu.edu.tw>
2022-07-09 09:48:31 +0000

    feat: padding static setting on du conf file

--- cmd/du-config.go
+++ cmd/du-config.go
@@ -36,14 +36,43 @@ func main() {
 	cuDomainName := "sample.com"
 	localInterfaceName := "ens3"
 
+	// config mnc
+	mod := "19c\\                  mnc = 93;"
+	cmd := exec.Command("sed", "-i", mod, "du_gnb.conf")
+	_, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set mnc: 93")
+	}
+
+	// config protc
+	mod = "195c\\    local_n_portc   = 601;"
+	cmd = exec.Command("sed", "-i", mod, "du_gnb.conf")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set local_n_protc: 601")
+	}
+
+	mod = "197c\\    remote_n_portc  = 600;"
+	cmd = exec.Command("sed", "-i", mod, "du_gnb.conf")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("[Err]", err)
+	} else {
+		fmt.Println("[LOG]Set remote_n_protc: 600")
+	}
+
 	// config local ip address
 	ip, err := GetInterfaceIpv4Addr(localInterfaceName)
 	if err != nil {
 		fmt.Println("[Err]", err)
 	}
 	fmt.Println("Config local interface ip: ", ip)
-	mod := "193c\\    local_n_address = \"" + ip + "\";"
-	cmd := exec.Command("sed", "-i", mod, "du_gnb.conf")
+	mod = "193c\\    local_n_address = \"" + ip + "\";"
+	cmd = exec.Command("sed", "-i", mod, "du_gnb.conf")
 	_, err = cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("[Err]", err)
